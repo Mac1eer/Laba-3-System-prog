8232a5cddea6367e7f79cda6e2db635f95cea6fa
Mac1eer <147583135+Mac1eer@users.noreply.github.com>
2024-09-19 09:45:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+try
+{
+    Process process = new Process();
+    for (int i = 0; i < 2; i++) { 
+    process.StartInfo.FileName = "notepad.exe";
+    Process.Start(process.StartInfo);
+}
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Произошла ошибка: " + ex.Message);
+}
